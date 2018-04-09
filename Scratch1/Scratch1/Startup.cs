d7697d061ce5a95cff13e7b8f8b669b22d71a27a
Liam Kelly <liamkell@microsoft.com>
2018-04-09 13:23:55 +0100

    enable swagger in prod

--- Scratch1/Scratch1/Startup.cs
+++ Scratch1/Scratch1/Startup.cs
@@ -38,13 +38,13 @@ namespace Scratch1
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+
+            }
                 app.UseSwagger();
                 app.UseSwaggerUI(c =>
                 {
                     c.SwaggerEndpoint("/swagger/v1/swagger.json", "demoapi");
                 });
-
-            }
 
             app.UseMvc();
         }
