7de3f776cc4040bcb103cf05d254b780c1597669
Liam Kelly <liamkell@microsoft.com>
2018-04-09 12:05:44 +0100

    Fist set of code comitted

--- Scratch1/Scratch1/Startup.cs
+++ Scratch1/Scratch1/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Scratch1
 {
@@ -24,6 +25,11 @@ namespace Scratch1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,6 +38,12 @@ namespace Scratch1
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "demoapi");
+                });
+
             }
 
             app.UseMvc();
